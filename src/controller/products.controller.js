cda1e06ea7f895bcb8186a2fc723f9cc233cc88c
AprendidendoGithub <R181308546@utna.edu.mx>
2024-03-20 16:43:44 -0600

    Adding post in the api

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -25,3 +25,26 @@ export const getProduct = async (req, res) =>{
     }
     res.status(200).send(result.recordsets[0]);
 }
+
+export const createProduct = async (req, res) =>{
+    const pool = await setConnection();
+    const result = await pool
+    .request()
+    .input("name", sql.VarChar, req.body.name)
+    .input("price", sql.Float, req.body.price)
+    .input("quantity", sql.Int, req.body.quantity)
+    .input("description", sql.Text, req.body.description)
+    .query("INSERT INTO products(name, price, quantity, description) VALUES(@name, @price, @quantity, @description); SELECT SCOPE_IDENTITY() AS id;")
+
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:"The product can't be created"})
+    }
+    res.status(200).json({
+        id: result.recordset[0].id,
+        name: req.body.name,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        description: req.body.description
+    })
+}
+
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,10 +1,12 @@
 import {Router} from 'express';
-import {getProducts, getProduct} from '../controller/products.controller.js'
+import {getProducts, getProduct, createProduct} from '../controller/products.controller.js'
 
 const router = Router();
 
 router.get("/products", getProducts)
 
 router.get("/product/:id", getProduct)
+
+router.post("/product", createProduct)
 
 export default router;
