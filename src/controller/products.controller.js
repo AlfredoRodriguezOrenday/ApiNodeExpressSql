ad1e659ab8866ae6049174fa013fb54a9c063d8e
AprendidendoGithub <R181308546@utna.edu.mx>
2024-03-20 16:52:25 -0600

    Adding updating in the api

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -48,3 +48,25 @@ export const createProduct = async (req, res) =>{
     })
 }
 
+export const updateProduct = async (req, res) =>{
+    const pool = await setConnection();
+    const result = await pool
+    .request()
+    .input("id", sql.Int, req.params.id)
+    .input("name", sql.VarChar, req.body.name)
+    .input("price", sql.Float, req.body.price)
+    .input("quantity", sql.Int, req.body.quantity)
+    .input("description", sql.Text, req.body.description)
+    .query("UPDATE products SET name = @name, price = @price, quantity = @quantity, description = @description WHERE id = @id")
+
+    if(result.rowsAffected[0] === 0){
+        return res.status(404).json({message:"The product can't be created"})
+    }
+    res.status(200).json({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        description: req.body.description
+    })
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,14 @@
 import {Router} from 'express';
-import {getProducts, getProduct, createProduct} from '../controller/products.controller.js'
+import {getProducts, getProduct, createProduct, updateProduct} from '../controller/products.controller.js'
 
 const router = Router();
 
-router.get("/products", getProducts)
+router.get("/products", getProducts);
 
-router.get("/product/:id", getProduct)
+router.get("/product/:id", getProduct);
 
-router.post("/product", createProduct)
+router.post("/product", createProduct);
+
+router.put("/product/:id", updateProduct);
 
 export default router;
